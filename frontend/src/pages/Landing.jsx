bb1123df51aec08ae065d50543abfd95ff02fb41
arslaan07 <arslaanbanday5@gmail.com>
2025-03-23 12:03:24 +0530

    adjusted padding for landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
-    <div className='bg-[#F7F8F9] flex justify-center items-end h-[100vh] px-[1.5rem] py-[2.5rem]'>
+    <div className='bg-[#F7F8F9] flex justify-center items-end min-h-[100vh] px-[1.5rem] pb-[3rem]'>
       <section className='flex flex-col gap-3'>
         <h1 className='font-semibold text-[28px]'>Welcome to PopX</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Pariatur repellat dolorem neque.</p>
             <Button asChild className='bg-[#6C25FF] max-w-[335px] h-[46px] hover:bg-[#5818e2]'><Link to="/signup">Create Account</Link></Button>
-            <Button asChild className='bg-[#6C25FF4B] max-w-[335px] h-[46px] text-[#1D2226] hover:bg-[#eee]'><Link to="/signin">Already Registered? Login</Link></Button>
+            <Button asChild className='bg-[#6C25FF4B] max-w-[335px] h-[46px] text-[#1D2226] hover:bg-[#eee] mb-[0.5rem]'><Link to="/signin">Already Registered? Login</Link></Button>
       </section>
     </div>
   )
